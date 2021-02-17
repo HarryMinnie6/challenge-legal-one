e1f13122704ee5edf848b74d96f86a572be051b9
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-02-18 00:03:38 +0200

    trying to deploy

--- app.js
+++ app.js
@@ -45,9 +45,9 @@ app.use((error, req, res, next) => {
 // serve static assets in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 
