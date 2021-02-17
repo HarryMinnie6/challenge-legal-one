97774edea872d511b9fd97ce7f1ef09d148c1d9d
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-02-17 23:57:56 +0200

    trying to deploy

--- app.js
+++ app.js
@@ -42,6 +42,13 @@ app.use((error, req, res, next) => {
   });
 });
 
-
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
 
 module.exports = app;
