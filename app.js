c2cf12e3f5cc7cfb19cf16fed8aa320d87696380
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-02-18 00:12:06 +0200

    trying to deploy

--- app.js
+++ app.js
@@ -42,13 +42,6 @@ app.use((error, req, res, next) => {
   });
 });
 
-// serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("./client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });
-}
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
