8ad68196f556cde90948a32b708735307452e5c4
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-02-17 22:55:18 +0200

    prepared for deployment

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 // this file spins up express aplication which makes handling requests easier for us.
 const express = require("express");
-
 const app = express();
-
 const morgan = require("morgan");
-
 const bodyParser = require("body-parser");
-
 const cors = require("cors");
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const path = require("path");
 
 // routes that handle requests
 const agentsRoute = require("./routes/agents");
@@ -44,5 +41,14 @@ app.use((error, req, res, next) => {
     },
   });
 });
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+
+
+
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
