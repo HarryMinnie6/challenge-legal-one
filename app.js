c05e5808f0623c68164d74e1014b76fc138ec2e9
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-02-18 13:18:56 +0200

    trying to deploy

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use("/resolution", resolutionRoute);
 app.use("/call", callsRoute);
 
 app.use("/", express.static("client/build"));
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
