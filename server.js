d65d1867748194054bd1ab792c8152bc44623b43
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-02-18 10:52:57 +0200

    trying to deploy

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const http = require("http");
 const app = require("./app");
 const express = require("express");
 const port = process.env.PORT || 5000;
-
+require('dotenv').config
 const server = http.createServer(app);
 
-// serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-  });
-}
+// // serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   //set static folder
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+//   });
+// }
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
